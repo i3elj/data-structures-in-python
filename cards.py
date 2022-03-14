452f90405092064c7c214e105eab89c7a0549bd8
i3elj <guisande.bernardo@gmail.com>
2022-03-14 15:05:45 -0300

    binary search algorithm

---
+++ cards.py
@@ -0,0 +1,33 @@
+def main():
+    arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print(locate_card(7, arr))
+    #number = 4
+    #arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    #pointer = len(arr) // 2
+    #iterations = 0
+    #while arr[pointer] != number:
+    #    if number > arr[pointer]:
+    #        arr = arr[pointer:]
+    #        pointer = len(arr) // 2
+    #    else:
+    #        arr = arr[:pointer]
+    #        pointer = len(arr) // 2
+    #    iterations += 1
+
+    #print("array:       ", arr)
+    #print("index:       ", pointer)
+    #print("value:       ", arr[pointer])
+    #print("number:      ", number)
+    #print("iterations:  ", iterations)
+
+def locate_card(query, cards):
+    pt = len(cards) // 2
+    if query == cards[pt]:
+        return pt
+    elif query > cards[pt]:
+        return locate_card(query, cards[pt:])
+    else:
+        return locate_card(query, cards[:pt])
+
+if __name__=="__main__":
+    main()
