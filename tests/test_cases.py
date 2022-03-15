f5055cd4710d45d0475ace62d5ef7740ea55ed3d
i3elj <guisande.bernardo@gmail.com>
2022-03-15 14:10:27 -0300

    commit

---
+++ tests/test_cases.py
@@ -0,0 +1,63 @@
+card_tests = []
+
+card_tests.append({
+    'name': "Cards Test",
+    'description': "When query occurs in the middle",
+    'input': {
+        'cards': [13, 11, 10, 7, 4, 3, 1, 0],
+        'query': 1
+    },
+    'output': 6
+})
+
+card_tests.append({
+    'name': "Cards Test",
+    'description': "When query is the first element",
+    'input': {
+        'cards': [4, 2, 1, -1],
+        'query': 4
+    },
+    'output': 5
+})
+
+card_tests.append({
+    'name': "Cards Test",
+    'description': "When the query is the last element",
+    'input': {
+        'cards': [3, -1, -9, -127],
+        'query': -127
+    },
+    'output': 3
+})
+
+card_tests.append({
+    'name': "Cards Test",
+    'description': "When the cards contains just one element",
+    'input': {
+        'cards': [6],
+        'query': 6
+    },
+    'output': 0
+ })
+
+card_tests.append({
+    'name': "Cards Test",
+    'description': "When the cards not contains the query",
+    'input': {
+        'cards': [9, 7, 5, 2, -9],
+        'query': 4
+    },
+    'output': -1
+})
+
+
+algo_tests = []
+
+algo_tests.append({
+    'description': "sample",
+    'input': {
+        'cards': ['s', 'a', 'm', 'p', 'l', 'e'],
+        'query': 's'
+    },
+    'output': 0
+})
--- tests/test_module.py
+++ tests/test_module.py
@@ -1,9 +1,25 @@
-from tests.test_variables import tests as tst
-from algorithms.cards import *
+from tests.test_cases import card_tests
+from algorithms.cards import linear
+from colorama import Fore
+
+
+def console(result, expected_output, index):
+    if result == expected_output:
+        print(f"{Fore.WHITE}{card_tests[index]['description']}")
+        print(f"{Fore.WHITE}{card_tests[index]['name']} {index}:", end=" ")
+        print(f"{Fore.GREEN}True", end="\n\n")
+    else:
+        print(f"{Fore.WHITE}{card_tests[index]['description']}")
+        print(f"{Fore.WHITE}{card_tests[index]['name']} {index}:", end=" ")
+        print(f"{Fore.RED}False", end="\n\n")
+
 
 def testing():
     index = 0
-    while index < len(tst):
-        result = linear(tst[index]['input']['query'], tst[index]['input']['cards'])
-        print(f"Test {index}: {result == tst[index]['output']}")
+    while index < len(card_tests):
+        result = linear(card_tests[index]['input']['query'],
+                        card_tests[index]['input']['cards'])
+        output = card_tests[index]['output']
+        # print(f"Test {index}: {result == card_tests[index]['output']}")
+        console(result, output, index)
         index += 1
